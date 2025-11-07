f5d553520a3aac653b58f0e9cf19ff9494363deb
Ueslei Paim <uesleibros@gmail.com>
2025-11-07 12:50:34 -0300

    Delete src/utils/logger.js

--- src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-/**
- * Simple logger utility.
- */
-export const logger = {
-  info: (...args) => console.info('[WixCMSSDK]', ...args),
-  warn: (...args) => console.warn('[WixCMSSDK]', ...args),
-  error: (...args) => console.error('[WixCMSSDK]', ...args),
-};
